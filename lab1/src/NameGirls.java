b69a6475d7d3331661a33718fa51ef66118b25a3
Najwa-Alawi-Alyafay <97404384+Najwa-Alawi-Alyafay@users.noreply.github.com>
2022-01-20 21:08:08 +0300

    Update NameGirls.java

--- lab1/src/NameGirls.java
+++ lab1/src/NameGirls.java
@@ -1,6 +1,42 @@
 /**
  * Created by TOSHIBA on 15/01/22.
  */
+import java.util.Scanner;
+
+/**
+ * Created by TOSHIBA on 15/01/22.
+ */
 public class NameGirls {
+    public static void main(String[] args)
+   {
+          Girls[] team=new Girls[3];
+
+                  // Girls s1=new Girls(1,"jojo");
+                 //Girls s2=new Girls(2,"joj");
+                //Girls s3=new Girls(3,"jo");
+               //team[0]=s1;
+              // team[2]=s2;
+             // team[3]=s3;
+       Scanner in=new Scanner(System.in);
+                  System.out.println("enter id & name");
+      for (int i = 0; i <team.length ; i++)
+    {
+        Girls x=new Girls(in.nextInt(),in.next());
+            team[i]=x;
+    }
+       for (int i = 0; i <team.length ; i++)
+    {
+                 System.out.println(team[i].toString());
+    }}}
+
+
+
+
+
+
+
+
+
+
 
 }
