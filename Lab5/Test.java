ff6cc95d7ad52cb300fc76282609c647c38a96be
Najwa-Alawi-Alyafay <97404384+Najwa-Alawi-Alyafay@users.noreply.github.com>
2022-03-11 09:36:19 +0300

    Add files via upload

---
+++ Lab5/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created by TOSHIBA on 19/02/22.
+ */
+public class Test {
+    public static void main(String[] args) {
+        SinglyLinkedList<String>list=new SinglyLinkedList<>();
+        System.out.println(list.size);
+    list.addfirst("Najwa");
+        list.addLast("Al-yafa");
+        System.out.println(list.size);
+while (!list.isEmpty()){
+    System.out.println(list.RemoveFirst());}
+    }}
