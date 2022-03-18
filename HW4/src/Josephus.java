2f17780c4b730aafeba17b78e137a00ed82ce108
Najwa-Alawi-Alyafay <97404384+Najwa-Alawi-Alyafay@users.noreply.github.com>
2022-03-18 19:34:42 +0300

    Add files via upload

---
+++ HW4/src/Josephus.java
@@ -0,0 +1,19 @@
+/**
+ * Created by TOSHIBA on 18/03/22.
+ */import java.io.*;
+
+public class Josephus {
+        static int josephus(int n, int k)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return (josephus(n - 1, k) + k - 1) % n + 1;
+        }
+        public static void main(String[] args) {
+            int n = 12;
+            int k = 2;
+            System.out.println("The  place is "
+                    + josephus(n, k));
+        }
+        }
