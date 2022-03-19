e4136b0b8b274cd8344b21984d9b84206b1b4eff
Najwa-Alawi-Alyafay <97404384+Najwa-Alawi-Alyafay@users.noreply.github.com>
2022-03-19 22:42:09 +0300

    Add files via upload

---
+++ HW3/src/Q1.java
@@ -0,0 +1,24 @@
+/**
+ * Created by TOSHIBA on 15/03/22.
+ */import java.io.*;
+import java.util.*;
+
+public class Q1 {
+    public static void main(String args[])
+        {
+            Stack<Integer> stack = new Stack<Integer>();
+            stack.push(10);
+            stack.push(15);
+            stack.push(30);
+            stack.push(20);
+            stack.push(5);
+            System.out.println("Initial Stack: " + stack);
+            System.out.println("Popped element: "
+                    + stack.pop());
+            System.out.println("Popped element: "
+                    + stack.pop());
+            System.out.println("Stack after pop operation "
+                    + stack);
+        }}
+
+
---
+++ HW3/src/Stack1.java
@@ -0,0 +1,11 @@
+
+
+public interface Stack1<E>{//إعلان عن الدوال
+    boolean isEmpty();
+    int size();
+    E top();
+    void push(E element);
+    E pop();
+
+}
+
